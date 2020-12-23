335e74f8251d2485755fb33e34d868d45e01bbdc
GammelJAN <jan.ehehalt@gmail.com>
2020-12-23 23:43:14 +0100

    assetsStructure - new tilesets - new Map started(Map1) - old files still in tiledMapDataOld

---
+++ core/assets/tiledmapData/tilesets/furniture.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="furniture" tilewidth="32" tileheight="32" tilecount="1408" columns="32">
+ <image source="../pngs/furniture.png" width="1024" height="1408"/>
+</tileset>
---
+++ core/assets/tiledmapData/tilesets/village.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="village" tilewidth="32" tileheight="32" tilecount="1800" columns="60">
+ <image source="../pngs/village.png" width="1920" height="960"/>
+</tileset>
